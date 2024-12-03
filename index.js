1125c33518944c52401dfec8691cfdf73f6f98d3
veshalsamaaro <veshal@samaaro.com>
2024-12-03 22:48:48 +0530

    add: phase one pool-widget

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = 3000;
+
+const pollData = [
+    {
+        "_id": 1,
+        "Question": "Which sport you like most?",
+        "options": [
+            {
+                "sport": "Cricket",
+                "vote": 7
+            },
+            {
+                "sport": "Football",
+                "vote": 8
+            },
+            {
+                "sport": "Basketball",
+                "vote": 5
+            },
+            {
+                "sport": "Tennis",
+                "vote": 6
+            },
+            {
+                "sport": "badminton",
+                "vote": 9
+            }
+        ]
+    }
+]
+app.use(express.json())
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Poll Widget' })
+})
+
+app.get('/get-poll-options', (req, res) => {
+    res.json({ ok: true, data: pollData})
+})
+
+app.post('/update-poll-option', (req, res) => {
+    const {sport, vote} = req.body;
+
+    let data = pollData;
+    data[0].options.forEach((option) => {
+        if (sport === option.sport) {
+            ++option.vote
+        }
+    })
+
+    return res.json({ ok: true, data});
+})
+
+app.listen(PORT, (req, res) => {
+    console.log("App Started on App:" + PORT);
+});
---
+++ public/script.js
@@ -0,0 +1,110 @@
+const quesContainer = document.querySelector('.question-container');
+const optDiv = document.querySelector('.option-div');
+const resetBtn = document.querySelector('.reset-btn');
+const submitBtn = document.querySelector('.submit-btn');
+
+let selectedOption = null;
+
+fetchPollData();
+
+async function fetchPollData() {
+    try {
+        const response = await fetch('/get-poll-options');
+        const responseData = await response.json();
+        if (responseData?.ok) {
+            const data = responseData?.data;
+
+            const question = data[0]?.Question;
+            const options = data[0]?.options;
+
+            optDiv.innerHTML = ""
+            quesContainer.innerHTML = ""
+
+            let QuestionTag = document.createElement('p');
+            QuestionTag.textContent = question;
+            quesContainer.append(QuestionTag);
+
+            let ulTag = document.createElement('ul');
+            options.forEach((option) => {
+                let liTag = document.createElement('li');
+                liTag.textContent = option?.sport;
+                liTag.value = option?.vote;
+                liTag.classList.add('option')
+                liTag.onclick = () => selectedData(option)
+                ulTag?.append(liTag);
+            })
+
+            optDiv.append(ulTag);
+        } else {
+            alert(responseData?.msg)
+            console.log(responseData)
+        }
+
+    } catch (error) {
+        console.error('Error >> ', error);
+        alert('Something went wrong');
+    }
+}
+
+function selectedData(option) {
+    selectedOption = option;
+}
+
+function resetSelectedOption () {
+    selectedOption = null;
+    fetchPollData();
+}
+
+
+async function updatePollData() {
+    console.log('clicked')
+    try {
+        if (selectedOption) {
+            const response = await fetch('/update-poll-option', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json', 
+                },
+                body: JSON.stringify(selectedOption)
+            });
+            const responseData = await response.json();
+            if (responseData?.ok) {
+                const data = responseData?.data;
+    
+                const question = data[0]?.Question;
+                const options = data[0]?.options;
+                optDiv.innerHTML = ""
+                quesContainer.innerHTML = ""
+                
+                let QuestionTag = document.createElement('p');
+                QuestionTag.textContent = question;
+                quesContainer.append(QuestionTag);
+                let ulTag = document.createElement('ul');
+                options.forEach((option) => {
+                    let liTag = document.createElement('li');
+                    let spanTag = document.createElement('span')
+                    liTag.textContent = option?.sport;
+                    liTag.value = option?.sport;
+                    liTag.classList.add('option')
+                    spanTag.classList.add('vote')
+                    spanTag.textContent = option?.vote;
+                    liTag.appendChild(spanTag)
+                    // liTag.onclick = () => selectedData(option)
+                    ulTag?.append(liTag);
+                })
+    
+                optDiv.append(ulTag);
+            } else {
+                alert(responseData?.msg)
+                console.log(responseData)
+            }
+        } else {
+            console.log("selected option is null ::: selectedOption ", selectedOption);
+            alert('Please select a option');
+        }
+
+    } catch (error) {
+        console.error('Error >> ', error);
+        alert('Something went wrong');
+    }
+}
